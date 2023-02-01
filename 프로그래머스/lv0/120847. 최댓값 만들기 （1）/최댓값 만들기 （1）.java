b9d890c3e59c52e5805ca1434219fb3452b4fa75
cornsilk-tea <dladndrbs@naver.com>
2023-02-01 21:31:25 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.48 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,13 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        List<Integer> list = new ArrayList<Integer>();
-        for(int num : numbers){
-            list.add(num);
-        }
-        Collections.sort(list, Collections.reverseOrder());
-        answer = list.get(0)*list.get(1);
-        return answer;
+        // int answer = 0;
+        // List<Integer> list = new ArrayList<Integer>();
+        // for(int num : numbers){
+        //     list.add(num);
+        // }
+        // Collections.sort(list, Collections.reverseOrder());
+        // answer = list.get(0)*list.get(1);
+        Arrays.sort(numbers);
+        return numbers[numbers.length-1]*numbers[numbers.length-2];
+        // return answer;
     }
 }

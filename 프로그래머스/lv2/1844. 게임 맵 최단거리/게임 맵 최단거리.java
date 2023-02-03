26f4650ba31244d368f4fa1cf1b80dd28e0b84d9
cornsilk-tea <dladndrbs@naver.com>
2023-02-03 14:54:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.34 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,18 +2,19 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int answer = 0;
+        // bfs 문제
         int[][] v = new int[maps.length][maps[0].length];
-        v[0][0] = 1;
-        bfs(maps, v);
+        bfs(v,maps);
         answer = v[maps.length-1][maps[0].length-1];
         if(answer == 0) answer = -1;
         return answer;
     }
-    public void bfs(int[][] maps, int[][] v){
+    public void bfs(int[][] v, int[][] maps){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0,0});
-        int[] dr = new int[]{-1,0,1,0};
-        int[] dc = new int[]{0,1,0,-1};
+        v[0][0] = 1;
+        int[] dr = new int[]{0,1,0,-1};
+        int[] dc = new int[]{-1,0,1,0};
         while(!q.isEmpty()){
             int[] current = q.remove();
             int cr = current[0];
@@ -21,7 +22,9 @@ class Solution {
             for(int i = 0; i < 4; i++){
                 int nr = cr + dr[i];
                 int nc = cc + dc[i];
-                if(nr < 0 || nc < 0 || nr >= maps.length || nc >= maps[0].length) continue;
+                if(nr < 0 || nc < 0 || nr >= maps.length || nc >= maps[0].length){
+                    continue;
+                }
                 if(v[nr][nc] == 0 && maps[nr][nc] == 1){
                     v[nr][nc] = v[cr][cc]+1;
                     q.add(new int[]{nr,nc});

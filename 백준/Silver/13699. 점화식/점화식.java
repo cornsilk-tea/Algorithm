e13154c8161e1555910615e214cf4944b947918f
cornsilk-tea <dladndrbs@naver.com>
2023-07-03 22:56:21 +0900

    [Silver IV] Title: 점화식, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[n+1];
+        dp[0] = 1;
+        for(int i = 1; i < dp.length; i++){
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i-j-1];
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}

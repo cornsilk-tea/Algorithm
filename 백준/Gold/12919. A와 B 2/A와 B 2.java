2c8eb671dd8f4e69fc98ae9ab6b32bc843ac404c
cornsilk-tea <dladndrbs@naver.com>
2023-07-05 22:40:41 +0900

    [Gold V] Title: A와 B 2, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -5,23 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(solution(br.readLine(), br.readLine()) ? 1 : 0);
+        System.out.println(r(br.readLine(), br.readLine()) ? 1 : 0);
     }
 
-    private static boolean solution(String s, String t) {
-        if(t.length() == s.length()){
-            return t.equals(s);
-        }
-        // 문자열 뒤에 A가 있다면 A를 빼준다.
-        if(t.endsWith("A") && solution(s, t.substring(0, t.length()-1))){
-            return true;
-        }
-        // 문자열 앞에 B가 있다면, 문자열을 뒤집고 맨 뒤의 B를 빼준다.
-        if(t.startsWith("B") && solution(s, new StringBuilder(t.substring(1)).reverse().toString())){
-            return true;
-        }
-
-        return false;
+    private static boolean r(String s, String t) {
+        return t.length() <= s.length() ? t.equals(s) : ((t.charAt(t.length()-1) == 'A' && r(s, t.substring(0, t.length()-1))) ||
+                (t.charAt(0) == 'B' && r(s, new StringBuilder(t.substring(1)).reverse().toString())));
     }
-
 }

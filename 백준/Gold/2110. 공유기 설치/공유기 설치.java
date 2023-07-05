ab611174e7a6ae1014041b2104dabc5d980b678d
cornsilk-tea <dladndrbs@naver.com>
2023-07-05 19:53:14 +0900

    [Gold IV] Title: 공유기 설치, Time: 312 ms, Memory: 28880 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -4,53 +4,51 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, C, minDistance;
-    static int[] map;
+    static int N, C;
+    static int[] houses;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        map = new int[N];
+        houses = new int[N];
         for(int i = 0; i < N; i++){
-            map[i] = Integer.parseInt(br.readLine());
+            houses[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(map);
-        boolean[] v = new boolean[N];
-        v[0] = true;
-        v[N-1] = true;
-        minDistance = -1;
-        // 우선 공유기는 끝에있는 두 집에는 무조건 설치된다?
-        // 그럼 C-2개를 내부에서 탐색하면 되나?
-        // 1 2 4 8 9
-        // 여기서 [2 4 8] 3개가 남았다고하고,
-        // 아니 그럼 이분탐색으로 뭘 찾으라는걸까?
-        // 이분탐색으로 거리를 찾으라는건가?
+        Arrays.sort(houses);
+        // 이분탐색으로 적절한 거리값을 탐색한다.
         // 탐색할 거리 범위 지정
-        // 시작 범위는 0
-        int start = 0;
-        // 끝 범위는 가장 끝값 - 첫값
-        int end = map[N-1] - map[0];
+        // 최소범위값는 0
+        int minDistance = 0;
+        // 최대범위값는 가장 끝값 - 첫값
+        int maxDistance = houses[N-1] - houses[0];
         int result = 0;
-        while(start <= end){
-            int mid = (start + end) / 2;
+        while(minDistance <= maxDistance){
+            int selectedDistance = (minDistance + maxDistance) / 2;
             // 가장 첫 위치는 무조건 공유기를 설치한다.
             int cnt = 1;
             // 거리 계산을 위해 이전집을 저장한다.
-            int prevHouse = map[0];
+            int prevHouse = houses[0];
             // 현재 탐색중인 거리 mid를 기준으로, 설치할 수 있는 집을 탐색
             for(int i = 1; i < N; i++){
-                if(map[i] - prevHouse >= mid){
+                if(houses[i] - prevHouse >= selectedDistance){
                     cnt++;
-                    prevHouse = map[i];
+                    prevHouse = houses[i];
                 }
             }
+            /*
+                C를 찾았더라도 계속 탐색하는 이유:
+                처음 찾은 저 값이 꼭 최적의 해라는 보장이 없기 때문에,
+                전체 탐색한 후 지속해서 최적값을 갱신해준다.
+             */
+            // 현재 설치 가능한 공유기 개수가 C개 이상이라면?
             if(cnt >= C){
-                result = mid;
-                start = mid + 1;
+                result = selectedDistance;
+                minDistance = selectedDistance + 1;
             }
+            // 현재 설치 가능한 공유기 개수가 C개 미만이라면?
             else {
-                end = mid - 1;
+                maxDistance = selectedDistance - 1;
             }
         }
         System.out.println(result);

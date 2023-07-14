71aee49b64ffe1c7d58cc0870f8a7d4234b8c79d
cornsilk-tea <dladndrbs@naver.com>
2023-07-15 00:36:54 +0900

    [Gold III] Title: 성곽, Time: 100 ms, Memory: 12316 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.java
+++ 백준/Gold/2234. 성곽/성곽.java
@@ -4,106 +4,89 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int[][] map, visited;
+    static int[][] map, areaMap;
     static int[] result;
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        /*
-            0. 이 성에 있는 방의 개수
-            1. 가장 넓은 방의 넓이
-            2. 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
-         */
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         result = new int[3];
         map = new int[M][N];
-        visited = new int[M][N];
+        areaMap = new int[M][N];
         for(int r = 0; r < M; r++){
             st = new StringTokenizer(br.readLine());
             for(int c = 0; c < N; c++){
                 map[r][c] = Integer.parseInt(st.nextToken());
             }
         }
-//        for(int[] a : map){
-//            System.out.println(Arrays.toString(a));
-//        }
-//        System.out.println("-----------------------------------");
-        // 왼쪽벽이 있으면 1을, 위쪽벽이 있으면 2를,
-        // 오른쪽 벽이 있으면 4를, 아래쪽 벽이 있으면 8
-        int number = 0;
-        List<Integer> areaList = new ArrayList<>();
+        // 0. 이 성에 있는 방의 개수
+        // 1. 가장 넓은 방의 넓이
+        // bfs를 돌면서 각 위치별 영역을 지정해주고, 영역별 사이즈도 저장해준다.
+        int areaNumber = 0;
+        List<Integer> areaSizeList = new ArrayList<>();
         for(int r = 0; r < M; r++){
             for(int c = 0; c < N; c++){
-                if(visited[r][c] == 0){
-                    areaList.add(bfs(r, c, ++number));
+                if(areaMap[r][c] == 0){
+                    int currAreaSize = bfs(r, c, ++areaNumber);
+                    areaSizeList.add(currAreaSize);
+                    result[1] = Math.max(result[1], currAreaSize);
                 }
             }
         }
-        // 0. 이 성에 있는 방의 개수
-        result[0] = number;
-//        for(int[] a : visited){
-//            System.out.println(Arrays.toString(a));
-//        }
+        result[0] = areaNumber;
+
         // 2. 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
-        // 이제 이걸 어떻게 구해야할지 생각해보자.
-        // 전체를 순차탐색하면서 해당하는 위치에서 어떤 벽을 없앨 수 있는지 확인하고,
-        // 벽을 지운 방향에 다른 영역이 존재하면, 그 영역의 값과 현재 영역의 값을 더해 최고값 갱신
         for(int r = 0; r < M; r++){
             for(int c = 0; c < N; c++){
                 for(int d = 0; d < 4; d++){
                     int nr = r + dr[d];
                     int nc = c + dc[d];
-                    if(nr < 0 || nc < 0 || nr >= visited.length || nc >= visited[0].length){
+                    // 다음 위치가 배열 범위를 초과하는지 확인
+                    // 현재 위치와 다음 위치가 같은 영역인지 확인
+                    if(isArrOut(nr,nc) || areaMap[r][c] == areaMap[nr][nc]){
                         continue;
                     }
-                    if(visited[r][c] != visited[nr][nc]){
-                        result[2] = Math.max(result[2], areaList.get(visited[r][c]-1) + areaList.get(visited[nr][nc]-1));
-                    }
+                    // areaSizeList에서 현재 위치 영역과 다음 위치 영역의 크기를 합쳐 정답 갱신
+                    result[2] = Math.max(result[2], areaSizeList.get(areaMap[r][c]-1) + areaSizeList.get(areaMap[nr][nc]-1));
                 }
             }
         }
-//        System.out.println("-----------------------------------");
-//        System.out.println(Arrays.toString(result));
         for(int i : result){
             System.out.println(i);
         }
     }
 
+    private static boolean isArrOut(int r, int c){
+        return r < 0 || c < 0 || r >= M || c >= N;
+    }
     static int[] dr = {-1, 0, 1, 0};  // 북, 동, 남, 서 순서
     static int[] dc = {0, 1, 0, -1};  // 북, 동, 남, 서 순서
-    static int[] heading = {2, 4, 8, 1};  // 북, 동, 남, 서 순서
+    static int[] wall = {2, 4, 8, 1};  // 북, 동, 남, 서 순서
 
 
-    private static int bfs(int R, int C, int number) {
-        int area = 1;
+    private static int bfs(int R, int C, int areaNumber) {
+        int areaSize = 1;
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{R,C});
-        visited[R][C] = number;
+        areaMap[R][C] = areaNumber;
         while(!q.isEmpty()){
             int[] curr = q.poll();
-            int r = curr[0];
-            int c = curr[1];
             for(int d = 0; d < 4; d++){
-                int nr = r + dr[d];
-                int nc = c + dc[d];
-                if(nr < 0 || nc < 0 || nr >= visited.length || nc >= visited[0].length){
+                int nr = curr[0] + dr[d];
+                int nc = curr[1] + dc[d];
+                // 현재 위치가 배열 범위 초과인지 확인.
+                // 현재 방향이 뚫린 방향인지 확인.
+                // 현재 위치가 다른 영역인지 확인
+                if(isArrOut(nr,nc) || (map[curr[0]][curr[1]] & wall[d]) != 0 || areaMap[nr][nc] != 0){
                     continue;
                 }
-                // 이쪽 방향이 뚫린 방향인지 확인.
-                if((map[r][c] & heading[d]) != 0){
-                    continue;
-                }
-                if(visited[nr][nc] != 0){
-                    continue;
-                }
-                visited[nr][nc] = number;
+                areaMap[nr][nc] = areaNumber;
                 q.add(new int[]{nr, nc});
-                area++;
+                areaSize++;
             }
         }
-        // 1. 가장 넓은 방의 넓이
-        result[1] = Math.max(result[1], area);
-        return area;
+        return areaSize;
     }
 }

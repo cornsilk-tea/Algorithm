4b4b05063eaf7d9c58cebe34866be665344971b4
cornsilk-tea <dladndrbs@naver.com>
2023-07-27 01:51:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 216 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static final int MOD = 1000000000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        // 길이가 i이고, 마지막 자릿수가 j인 계단 수의 개수를 저장
+        int[][] arr = new int[N+1][10];
+        int answer = 0;
+        // 1의자리수 초기화
+        // 0으로 시작하는 수는 계단수가 아니므로 제외
+        for(int i = 1; i < 10; i++){
+            arr[1][i] = 1;
+        }
+        // 나머지 초기화
+        for(int i = 2; i <= N; i++){
+            for(int j = 0; j < 10; j++){
+                // 계단수는 마지막 숫자기준 -1 또는 +1만 가능하다.
+                // 0은 -1이 불가능하기에 +1만 가져온다.
+//                if(j == 0){
+//                    arr[i][j] = arr[i-1][j+1] % MOD;
+//                }
+//                // 9는 +1이 불가능하기에 -1만 가져온다.
+//                else if(j == 9){
+//                    arr[i][j] = arr[i-1][j-1] % MOD;
+//                }
+//                // 0과 9를 제외한 나머지는 -1과 +1 모두 가능하기에 모두 가져온다.
+//                else{
+//                    arr[i][j] = (arr[i - 1][j - 1] + arr[i - 1][j + 1]) % MOD;
+//                }
+                arr[i][j] = (
+                        (j == 0 ? 0 : arr[i - 1][j - 1]) +
+                        (j == 9 ? 0 : arr[i - 1][j + 1])
+                ) % MOD;
+
+            }
+        }
+        for(int i = 0; i <= 9; i++){
+            answer = (answer + arr[N][i]) % MOD;
+        }
+        System.out.println(answer);
+    }
+
+}

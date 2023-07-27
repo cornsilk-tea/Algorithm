3102cfd3e58dab21b463d74c9e2a18fcfb331a76
cornsilk-tea <dladndrbs@naver.com>
2023-07-27 18:58:35 +0900

    [Gold IV] Title: 여행 가자, Time: 204 ms, Memory: 18560 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] cityConections;
+    static int[] cityRoots, travelPlan;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        cityConections = new int[N+1][N+1];
+        travelPlan = new int[M + 1];
+        cityRoots = new int[N+1];
+        StringTokenizer st;
+        for(int r = 1; r <= N; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c = 1; c <= N; c++){
+                cityConections[r][c] = Integer.parseInt(st.nextToken());
+            }
+            cityRoots[r] = r;
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= M; i++){
+            travelPlan[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int r = 1; r <= N; r++){
+            for(int c = 1; c <= N; c++){
+                if(cityConections[r][c] == 1){
+                    union(r, c);
+                }
+            }
+        }
+        int firstCityRoot = find(travelPlan[1]);
+        for(int i = 2; i < travelPlan.length; i++){
+            if(firstCityRoot != find(travelPlan[i])){
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+    }
+
+    private static void union(int city1, int city2) {
+        city1 = find(city1);
+        city2 = find(city2);
+        if(city1 != city2){
+            cityRoots[city2] = city1;
+        }
+    }
+
+    private static int find(int city) {
+        return city == cityRoots[city] ? city : (cityRoots[city] = find(cityRoots[city]));
+    }
+}

9b1bf7fc7a7c0fdc67a3d7ded7ef08b976c572c6
cornsilk-tea <dladndrbs@naver.com>
2023-09-12 23:54:27 +0900

    [Silver II] Title: 알파벳 블록, Time: 884 ms, Memory: 264316 KB -BaekjoonHub

---
+++ 백준/Silver/27497. 알파벳 블록/알파벳 블록.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Deque<Character> deque = new ArrayDeque<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int n = 0; n < N; n++){
+            st = new StringTokenizer(br.readLine());
+            if(st.countTokens() < 2){
+                // 무조건 3임
+                if(!stack.empty()) {
+                    if(stack.pop() == 1) {
+                        // 맨 뒤 문자 제거
+                        deque.pollLast();
+                    }
+                    else {
+                        // 맨 앞 문자 제거
+                        deque.pollFirst();
+                    }
+                }
+            }
+            else{
+                // 문자열 맨 뒤에 c가 적힌 블록 추가
+                stack.push(Integer.parseInt(st.nextToken()));
+                char c = st.nextToken().charAt(0);
+                if(stack.peek() == 1){
+                    deque.addLast(c);
+                }
+                else{
+                    deque.addFirst(c);
+                }
+            }
+        }
+        System.out.println(deque.size() == 0 ? "0" : printDeque(deque));
+    }
+
+    private static String printDeque(Deque<Character> deque) {
+        StringBuilder sb = new StringBuilder();
+        while(!deque.isEmpty()){
+            sb.append(deque.pollFirst());
+        }
+        return sb.toString();
+    }
+}

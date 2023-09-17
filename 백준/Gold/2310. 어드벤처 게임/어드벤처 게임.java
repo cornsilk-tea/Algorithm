7e2a120d8897a4f12fccd79b63f813faf730973b
cornsilk-tea <dladndrbs@naver.com>
2023-09-18 00:52:39 +0900

    [Gold IV] Title: 어드벤처 게임, Time: 1292 ms, Memory: 64072 KB -BaekjoonHub

--- 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
+++ 백준/Gold/2310. 어드벤처 게임/어드벤처 게임.java
@@ -30,7 +30,7 @@ public class Main {
             if (n == 0) break;
             // 금액으로 방문체크
             visited = new boolean[n][501];
-            rooms = new LinkedList<>();
+            rooms = new ArrayList<>();
 
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
@@ -46,38 +46,81 @@ public class Main {
         }
     }
 
-    private static boolean solve(int idx, int money) {
-        Room room = rooms.get(idx);
-        int tempMoney = money;
-        if (idx == n - 1) {
-            // 여기서도 계산 한번 해줘서 갈 수 있는지 판단해야한다.
+    private static boolean solve(int startIdx, int startMoney) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{startIdx, startMoney});
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int idx = current[0];
+            int money = current[1];
+            Room room = rooms.get(idx);
+
+            if (idx == n - 1) {
+                if (room.type == 'T') {
+                    money -= room.money;
+                    if (money < 0) {
+                        continue;
+                    }
+                }
+                return true;
+            }
+
             if (room.type == 'T') {
-                tempMoney -= room.money;
-                if (tempMoney < 0) {
-                    return false;
+                money -= room.money;
+                if (money < 0) {
+                    continue;
+                }
+            } else if (room.type == 'L') {
+                if (money < room.money) {
+                    money = room.money;
                 }
             }
-            return true;
-        }
-        if (room.type == 'T') {
-            tempMoney -= room.money;
-            if (tempMoney < 0) {
-                return false;
-            }
-        } else if (room.type == 'L') {
-            if (tempMoney < room.money) {
-                tempMoney = room.money;
-            }
-        }
-        // 다음에 갈곳 지정
-        for (int nextIdx : room.connectedRooms) {
-            if (visited[nextIdx][tempMoney] == false) {  // 이전에 이 금액으로 방문하지 않았다면
-                visited[nextIdx][tempMoney] = true;  // 방문 체크
-                if (solve(nextIdx, tempMoney)) {  // 다음 방으로 이동
-                    return true;
+
+            for (int nextIdx : room.connectedRooms) {
+                if (!visited[nextIdx][money]) {
+                    visited[nextIdx][money] = true;
+                    queue.offer(new int[]{nextIdx, money});
                 }
             }
         }
+
         return false;
     }
+
+//    private static boolean solve(int idx, int money) {
+//        Room room = rooms.get(idx);
+//        int tempMoney = money;
+//        if (idx == n - 1) {
+//            // 여기서도 계산 한번 해줘서 갈 수 있는지 판단해야한다.
+//            // 이거 안해줘서 1번이 계속 틀렸다.
+//            if (room.type == 'T') {
+//                tempMoney -= room.money;
+//                if (tempMoney < 0) {
+//                    return false;
+//                }
+//            }
+//            return true;
+//        }
+//        if (room.type == 'T') {
+//            tempMoney -= room.money;
+//            if (tempMoney < 0) {
+//                return false;
+//            }
+//        } else if (room.type == 'L') {
+//            if (tempMoney < room.money) {
+//                tempMoney = room.money;
+//            }
+//        }
+//        // 다음에 갈곳 지정
+//        for (int nextIdx : room.connectedRooms) {
+//            if (visited[nextIdx][tempMoney] == false) {  // 이전에 이 금액으로 방문하지 않았다면
+//                visited[nextIdx][tempMoney] = true;  // 방문 체크
+//                if (solve(nextIdx, tempMoney)) {  // 다음 방으로 이동
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 }
